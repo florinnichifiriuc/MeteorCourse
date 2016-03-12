a27f2f171f49de1f4c3ae9fe295dea6362820346
Florin Nichifiriuc <florinnichifiriuc@gmail.com>
2016-03-12 15:30:21 +0200

    Add gulp file to start a service. Add Brwser sync.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MeteorCourse
 Meteor JS
+
+## Install:
+Install node and npm, then *run npm install* in the current directory.
+Run npm install -g gulp.
+
+Run gulp serve.
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var filesToWatch =[
+"./**/*.css",
+"./**/*.js",
+"./**/*.html",
+];
+// Static Server + watching css/js/html files
+gulp.task('serve',  function() {
+
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch(filesToWatch).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
